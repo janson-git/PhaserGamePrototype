6e004dbc41c7adb660cc7015bc9a78ccb119b18f
Ivan Lisitskiy <ivan.lisitskiy@dev-ifc.com>
2020-09-16 19:51:06 +0300

    GameScene remove unused code, upload some tiles

--- app/src/scenes/GameScene.ts
+++ app/src/scenes/GameScene.ts
@@ -7,6 +7,8 @@ import StaticTilemapLayer = Phaser.Tilemaps.StaticTilemapLayer;
 import BlendModes = Phaser.BlendModes;
 import WaterMazeTilesProcessor from "../lib/WaterMaze/WaterMazeTilesProcessor";
 import {BoatTrail} from "../Components/BoatTrail";
+import Tilemap = Phaser.Tilemaps.Tilemap;
+import Tileset = Phaser.Tilemaps.Tileset;
 
 export class GameScene extends Phaser.Scene {
 
@@ -47,8 +49,6 @@ export class GameScene extends Phaser.Scene {
 
         this.load.image('tilesExtruded', 'assets/tilemaps/WaterMazeTilesExtruded.png');
         this.load.tilemapTiledJSON('map', 'assets/tilemaps/WaterMazeMap.json');
-        // this.load.image('tilesExtruded16', 'assets/tilemaps/WaterGrassTiles16Extruded.png');
-        // this.load.tilemapTiledJSON('map', 'assets/tilemaps/WaterGrassMap16.json');
     }
 
     public create() {
@@ -72,7 +72,6 @@ export class GameScene extends Phaser.Scene {
 
             map = this.make.tilemap({data: level, tileWidth: 24, tileHeight: 24});
             tiles = map.addTilesetImage('waterAndGrass', 'tilesExtruded', 26, 26, 1, 1, 1);
-            // tiles = map.addTilesetImage('waterAndGrass16', 'tilesExtruded16', 16, 16, 1, 2, 1);
             this.layer = map.createStaticLayer(0, tiles, 0, 0);
 
             // а теперь для расставленных тайлов установим обработку коллизий
@@ -87,8 +86,7 @@ export class GameScene extends Phaser.Scene {
 
             // The first parameter is the name of the tileset in Tiled and the second parameter is the key
             // of the tileset image used when loading the file in preload.
-            // let tiles = map.addTilesetImage('waterAndGrass', 'tilesExtruded');
-            tiles = map.addTilesetImage('waterAndGrass16', 'tilesExtruded16');
+            tiles = map.addTilesetImage('waterAndGrass', 'tilesExtruded');
 
             // You can load a layer from the map using the layer name from Tiled, or by using the layer
             // index (0 in this case).
@@ -152,24 +150,7 @@ export class GameScene extends Phaser.Scene {
         this.scoreText.setScrollFactor(0);
 
         // Create minimap
-        let miniMapContainer = this.add.group();
-        let gameScale = this.sys.game.scale;
-        // background
-        let miniMapBg = this.add.rectangle(70, gameScale.height - 70, 130, 130, 0x666666, 0.6);
-        miniMapBg.setScrollFactor(0);
-
-        // SCALED tilemap on background, Scale = (1 / tileWidth)
-        this.miniLayer = miniMap.createStaticLayer(0, tiles, 10, gameScale.height - 130);
-        this.miniLayer.setScale(this.MINIMAP_SCALE);
-        this.miniLayer.setScrollFactor(0);
-        this.miniLayer.setBlendMode(BlendModes.SCREEN);
-
-        // graphics to draw player and other objects!
-        this.miniMapLocator = this.add.graphics();
-        this.miniMapLocator.setScrollFactor(0);
-        this.miniMapLocator.setPosition(10, gameScale.height - 130, 130, 130);
-
-        miniMapContainer.addMultiple([miniMapBg, this.miniLayer, this.miniMapLocator]);
+        this.createMiniMap(miniMap, tiles);
     }
 
     public update(time, delta) {
@@ -216,64 +197,24 @@ export class GameScene extends Phaser.Scene {
         }
     }
 
-
-    private renderTexture: Phaser.GameObjects.RenderTexture;
-    private unitDots: Phaser.GameObjects.Graphics;
-
-    public createMiniMap(level) {
+    public createMiniMap(miniMap: Tilemap, tiles: Tileset) {
         let miniMapContainer = this.add.group();
-
         let gameScale = this.sys.game.scale;
+        // background
+        let miniMapBg = this.add.rectangle(70, gameScale.height - 70, 130, 130, 0x666666, 0.6);
+        miniMapBg.setScrollFactor(0);
 
-        let miniMap = this.add.rectangle(60, gameScale.height - 60, 100, 100, 0x666666, 0.8);
-        miniMap.setScrollFactor(0);
-        // let resolution = 2 / 100;
-        // let renderWH = 120 * 16; // ширина карты в целом 120 тайлов * 16 пкс на тайл
-        //
-        // let gameScale = this.sys.game.scale;
-        // this.renderTexture = this.add.renderTexture(renderWH, renderWH);
-        // this.renderTexture.scale = resolution;
-        // var cropRect = new Phaser.Geom.Rectangle(0, 0, 200, 200);
-        // this.renderTexture.setCrop(cropRect);
-        // var miniMapY = this.cameras.main.height - (gameScale.height * resolution);
-        // var miniMapUI = this.add.image(0, 0, 'mini_map');
-        // this.renderTexture.displayWidth = this.renderTexture.scale * gameScale.width;
-        // this.renderTexture.displayHeight = this.renderTexture.scale * gameScale.height;
-        // var cropRect = new Phaser.Geom.Rectangle(0, 0, this.renderTexture.displayWidth, this.renderTexture.displayHeight);
-        // this.renderTexture.setCrop(cropRect);
-        // var miniWidth = .075 * this.renderTexture.displayWidth + 100;
-        // var miniHeight = miniMapY - (.06 * this.renderTexture.displayHeight);
-        // let miniMap = this.add.sprite(miniWidth, miniHeight, this.renderTexture.texture);
-        // var padding = .241 * this.renderTexture.displayHeight;
-        // miniMapUI.width = (this.renderTexture.displayWidth + padding);
-        // miniMapUI.height = (this.renderTexture.displayHeight + padding);
-        // miniMapUI.y = this.cameras.main.height - miniMapUI.height;
-        // miniMapUI.setScrollFactor(0);
-        // miniMap.setScrollFactor(0);
-        //
-        // let viewRect = this.add.graphics();
-        // viewRect.lineStyle(1, 0xFFFFFF);
-        // viewRect.fillRect(miniMap.x, miniMap.y, this.cameras.main.width * resolution, this.cameras.main.height * resolution);
-        //
-        // this.unitDots = this.add.graphics({x: miniMap.x, y: miniMap.y});
-        // // this.unitDots.setScrollFactor(0);
-        // var bg = this.add.graphics();
-        // // bg.fillStyle(0x000000, 1);
-        // // bg.fillRect(0, miniMapUI.y + (miniMapUI.height * .1), miniMapUI.width * .95, miniMapUI.height * .9);
-        // // bg.setScrollFactor(0);
-        // var children = [bg, miniMap, this.unitDots, viewRect, miniMapUI];
-        miniMapContainer.addMultiple([miniMap]);
-    }
+        // SCALED tilemap on background, Scale = (1 / tileWidth)
+        this.miniLayer = miniMap.createStaticLayer(0, tiles, 10, gameScale.height - 130);
+        this.miniLayer.setScale(this.MINIMAP_SCALE);
+        this.miniLayer.setScrollFactor(0);
+        this.miniLayer.setBlendMode(BlendModes.SCREEN);
 
-    private updateUnitDots() {
-        this.unitDots.clear();
-        this.stars.getChildren().forEach((object) => {
-            var unitMiniX = object.body.position.x * this.renderTexture.scale;
-            var unitMiniY = object.body.position.y * this.renderTexture.scale;
+        // graphics to draw player and other objects!
+        this.miniMapLocator = this.add.graphics();
+        this.miniMapLocator.setScrollFactor(0);
+        this.miniMapLocator.setPosition(10, gameScale.height - 130, 130, 130);
 
-            var color = 0x1331a1;
-            this.unitDots.fillStyle(color, 1);
-            this.unitDots.fillEllipse(unitMiniX, unitMiniY, 2, 2);
-        });
+        miniMapContainer.addMultiple([miniMapBg, this.miniLayer, this.miniMapLocator]);
     }
 }
