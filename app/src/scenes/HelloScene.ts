c017eb3b5d11b0dd0bd4e6b5894ef66a9238794e
Ivan Lisitskii <ivan.janson@gmail.com>
2024-11-09 18:43:58 +0300

    add version info on start screen

--- app/src/scenes/HelloScene.ts
+++ app/src/scenes/HelloScene.ts
@@ -2,6 +2,8 @@ import * as Phaser from "phaser";
 import GameMenuButton from "../Components/GameMenuButton";
 import {SceneBase} from "./SceneBase";
 
+declare const VERSION;
+
 export class HelloScene extends SceneBase {
 
     constructor() {
@@ -23,7 +25,7 @@ export class HelloScene extends SceneBase {
         let centerX = this.gameWidth / 2;
         let centerY = this.gameHeight / 2;
 
-        let header = this.add.text(centerX, 20, 'Select a mode');
+        let header = this.add.text(centerX, 20, 'SELECT MODE');
         header.setFontStyle('bold')
             .setFontSize(16)
             .setX(centerX - Math.round(header.width / 2));
@@ -42,6 +44,16 @@ export class HelloScene extends SceneBase {
             this.scene.start('Generator');
         });
         this.add.existing(goMazeGenerator);
+
+        let textStyle = {fontSize: '11px', fill: '#fff', fontFamily: 'Arial, sans-serif'};
+        let versionText = this.add.text(
+                centerX,
+                this.gameHeight - 20,
+                'Version: ' + VERSION,
+                textStyle
+            )
+            .setScrollFactor(0);
+        versionText.setX(centerX - Math.round(versionText.width / 2))
     }
 
     public update(time, delta) {
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -3,6 +3,16 @@ const path = require('path');
 const webpack = require('webpack');
 const CopyPlugin = require('copy-webpack-plugin');
 
+const e = new Date();
+const Y = e.getFullYear();
+let M = (e.getMonth() + 1).toString().padStart(2, '0');
+let D = e.getDate().toString().padStart(2, '0');
+let h = e.getHours().toString().padStart(2, '0');
+let m = e.getMinutes().toString().padStart(2, '0');
+let s = e.getSeconds().toString().padStart(2, '0');
+
+const versionString = '' + Y + M + D + 'T' + h + m + s;
+
 module.exports = {
     entry: {
         app: './src/App.ts',
@@ -50,6 +60,7 @@ module.exports = {
         new webpack.DefinePlugin({
             'typeof CANVAS_RENDERER': JSON.stringify(true),
             'typeof WEBGL_RENDERER': JSON.stringify(true),
+            VERSION: JSON.stringify(versionString)
         }),
     ],
 
